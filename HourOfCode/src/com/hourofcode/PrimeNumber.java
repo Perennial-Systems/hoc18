9be6233caa532a9a4972c69dbfb713bec67c94de
prasanna wagh <prasanna.wagh14@gmail.com>
2018-12-05 19:18:46 +0530

    change method name

--- HourOfCode/src/com/hourofcode/PrimeNumber.java
+++ HourOfCode/src/com/hourofcode/PrimeNumber.java
@@ -10,26 +10,20 @@ public class PrimeNumber {
 	public static void main(String[] args) {
 		int firstNo = 1;
 		int lastNo = 100;
-		printPrimeNumber(firstNo, lastNo);
+		isPrime(firstNo, 2, lastNo);
 		System.out.println(primeList);
 	}
 
-	private static void printPrimeNumber(int firstNo, int lastNo) {
-		prime(firstNo, 2, lastNo);
-	}
-
-	static void prime(int sampleInput, int i, int lastNo) {
+	static void isPrime(int sampleInput, int i, int lastNo) {
 		if (sampleInput <= lastNo) {
 			if (i < sampleInput) {
 				if (sampleInput % i != 0) {
-					prime(sampleInput, ++i, lastNo);
-				} else {
-
+					isPrime(sampleInput, ++i, lastNo);
 				}
 			}
 			primeList.add(sampleInput);
 			++sampleInput;
-			prime(sampleInput, 2, lastNo);
+			isPrime(sampleInput, 2, lastNo);
 		}
 	}
 }
