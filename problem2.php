8404e584f78f18f4b8f4b477508e45dfc6985b3c
Sushant Pimple <sushant.pimple@perennialsys.com>
2018-12-05 19:18:54 +0530

    Problem 2 incomplete

---
+++ problem2.php
@@ -0,0 +1,16 @@
+<?php
+$str = "adsaasds545 dsaddasda1231 23123";
+$exploded_str = explode($str, ' ');
+var_dump($exploded_str);
+foreach ($exploded_str as $item){
+    $sum = 0;
+    $str_ary = str_split($item,1);
+    foreach ($str_ary as $key => $val) {
+        if(filter_var($val, FILTER_VALIDATE_INT)){
+            $sum+= $val;
+        }
+    }
+    var_dump($sum);
+}
+
+?>
