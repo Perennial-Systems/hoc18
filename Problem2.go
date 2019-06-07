206f65b70446c950ae272949ca6696b7d8f63f65
nikhil <anandnikhil91@gmail.com>
2019-06-07 15:44:13 +0530

    Problem1 done

---
+++ Problem2.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	typed := "aWk60#ase2> o_0^4alis5L" //Hardcoded can be removed by removing following commented statements
+	//fmt.Println("Enter the encoded string:")
+	//fmt.Scan(&typed)
+
+	brokenTyped := strings.Fields(typed)
+
+	for i := 0; i < len(brokenTyped); i++ {
+		j := decode(brokenTyped[i])
+		if j == 0 {
+			j = 32
+		} else {
+			j += 96 //to make 1 == ascii(97) is 'a'
+		}
+		fmt.Print(string(j))
+	}
+}
+
+//ascii 48 to 57 is 0 to 9
+//32 ascii is space
+//97 is a
+
+func decode(x string) int {
+	j := 0
+	for i := 0; i < len(x); i++ {
+		if x[i] >= 48 && x[i] <= 57 {
+			j += int(x[i]) - 48
+		}
+	}
+	return j
+}
