bc0845bdd8f17d97caecdf0d731fa1f93554334d
pawas <sethpawas2950@gmail.com>
2019-06-10 15:39:10 +0530

    commit

--- decode2.go
+++ decode2.go
@@ -10,7 +10,7 @@ func main() {
 
 	fmt.Println("Enter Two Strings seprating a space ")
 	var s1, s2 string
-	fmt.Scanf("%s %s", &s1, &s2)
+	_, _ = fmt.Scanf("%s %s", &s1, &s2)
 
 	re := regexp.MustCompile("[0-9]+")
 	str1 := make([]string, 100)
@@ -19,30 +19,27 @@ func main() {
 	for i := 0; i < len(str1); i++ {
 		k, _ := strconv.Atoi(str1[i])
 		sum1 += k
+		sum1 = sum1 % 26
 	}
 	//fmt.Println(sum1)
-	fmt.Println(string(sum1 + 96))
-
+	if sum1 == 0 {
+		fmt.Print("No number in a string")
+	} else {
+		fmt.Print(string(sum1 + 96))
+	}
 	str2 := make([]string, 100)
 	str2 = re.FindAllString(s2, -1)
 	sum2 := 0
 	for j := 0; j < len(str2); j++ {
 		k, _ := strconv.Atoi(str2[j])
 		sum2 += k
+		sum2 = sum2 % 26
 	}
 	//fmt.Println(sum2)
-	fmt.Println(string(sum2 + 96))
+	if sum2 == 0 {
+		fmt.Print(string(sum2 + 96))
+	} else {
+		fmt.Println("No number in a string")
+	}
 
-	//arr := make([]string, 100)
-	//w1 := arr[0]
-	//w2 := arr[]
-	//len(w1) w1[1]
-
-	//var i, num int
-	//
-	//for i, num := range str1 {
-	//	var sum1 int
-	//	sum1 = 0
-	//	sum1 = sum1 + []int(str1[i])
-	//}
 }
